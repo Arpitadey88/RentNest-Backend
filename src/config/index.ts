eab52ebc7142e43277502325b6d57f5ee045119d
Arpita Banik <deyarpitabanik@gmail.com>
2026-08-30 23:19:32 +0600

    changed in index

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,6 +16,6 @@ export default {
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
   jwt_refresh_expiries_in: process.env.JWT_REFRESH_EXPIRES_IN,
   stripe_secret_key: process.env.STRIPE_SECRET_KEY,
-  // stripe_product_price_id: process.env.STRIPE_PRODUCT_PRICE_KEY,
+
   stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
 };
